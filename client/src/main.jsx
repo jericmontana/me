51270f12bc67605d1cbf07e2bfb797d051f8245f
jericmontana <mntnjrc@gmail.com>
2022-07-23 22:31:15 +0800

    The Foundation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import '../src/assets/prod/css/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <h1 className="h0">Jeric Montana</h1>
+    <h1 className="h2">Jeric Montana</h1>
   </React.StrictMode>
 )
